b76cfe7e801493782b2b7a56aa9eb3056fb60292
trishagarniss <trishagarniss@gmail.com>
2026-05-06 20:02:47 +0700

    fix: crypto_aes cdc -> gcm

--- backend/crypto_aes.py
+++ backend/crypto_aes.py
@@ -1,37 +1,28 @@
 import hashlib
 import os
-from Crypto.Cipher import AES
-from Crypto.Util.Padding import pad, unpad
-from Crypto.Random import get_random_bytes
+from cryptography.hazmat.primitives.ciphers.aead import AESGCM
 
 # HASHING (SHA-256)
 def sha256_hash(data: bytes) -> str:
     return hashlib.sha256(data).hexdigest()
 
-
 def hash_file(filepath: str) -> str:
     with open(filepath, 'rb') as f:
         return sha256_hash(f.read())
 
-# XOR DYNAMIC KEY
+# DYNAMIC KEY GENERATION (XOR)
 def xor_hex(hex1: str, hex2: str) -> bytes:
-    """
-    XOR dua string hex dan return bytes (32 byte = 256 bit)
-    """
+    # XOR dua string hex dan return bytes (32 byte = 256 bit)
     b1 = bytes.fromhex(hex1)
     b2 = bytes.fromhex(hex2)
 
-    # pastikan panjang sama
     length = min(len(b1), len(b2))
     result = bytes([b1[i] ^ b2[i] for i in range(length)])
 
     return result
 
-
 def generate_dynamic_key(file_hash: str, prev_hash: str) -> bytes:
-    """
-    Generate AES-256 key dari XOR hash file dan prev hash
-    """
+    # Generate AES-256 key dari XOR hash file dan prev hash
     key = xor_hex(file_hash, prev_hash)
 
     # pastikan 32 byte (AES-256)
@@ -42,36 +33,35 @@ def generate_dynamic_key(file_hash: str, prev_hash: str) -> bytes:
 
     return key
 
-
-# ENCRYPTION
+# ENCRYPTION (AES-256-GCM)
 def encrypt_file(input_path: str, key: bytes, output_path: str):
-    """
-    Encrypt file menggunakan AES CBC
-    """
+    # Encrypt file menggunakan AES-256-GCM sesuai arsitektur CloudGuard EMT.
     with open(input_path, 'rb') as f:
         data = f.read()
 
-    iv = get_random_bytes(16)
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-
-    ciphertext = cipher.encrypt(pad(data, AES.block_size))
+    aesgcm = AESGCM(key)
+    
+    nonce = os.urandom(12)
+    
+    ciphertext = aesgcm.encrypt(nonce, data, None)
 
     with open(output_path, 'wb') as f:
-        f.write(iv + ciphertext)
+        f.write(nonce + ciphertext)
 
     return output_path
 
-
-# DECRYPTION
+# DECRYPTION (AES-256-GCM)
 def decrypt_file(input_path: str, key: bytes, output_path: str):
     with open(input_path, 'rb') as f:
         file_data = f.read()
 
-    iv = file_data[:16]
-    ciphertext = file_data[16:]
+    # Pisahkan 12 byte pertama sebagai Nonce, sisanya adalah Ciphertext + Tag
+    nonce = file_data[:12]
+    ciphertext = file_data[12:]
 
-    cipher = AES.new(key, AES.MODE_CBC, iv)
-    plaintext = unpad(cipher.decrypt(ciphertext), AES.block_size)
+    aesgcm = AESGCM(key)
+    
+    plaintext = aesgcm.decrypt(nonce, ciphertext, None)
 
     with open(output_path, 'wb') as f:
         f.write(plaintext)
