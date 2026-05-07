67abd99f05024e0eb4b144c366c7c07568761fd2
trishagarniss <trishagarniss@gmail.com>
2026-05-08 03:40:57 +0700

    done

--- security_app/urls.py
+++ security_app/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('audit/<int:doc_id>/', views.audit_document, name='audit_document'),
     path('documents/', views.get_all_documents, name='get_all_documents'),
     path('audit-network/', views.audit_network_integrity, name='audit_network'),
+    path('download/<int:doc_id>/', views.download_decrypted_file, name='download_decrypted_file'),
 ]
