2165e6e724078bbf6ca079f92f514bce5bb26d86
trishagarniss <trishagarniss@gmail.com>
2026-05-08 03:44:20 +0700

    edit

--- cloudguard_prj/settings.py
+++ cloudguard_prj/settings.py
@@ -41,10 +41,12 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'corsheaders',
     'security_app',
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -137,3 +139,5 @@ MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media' # Lokasi file ciphertext (.bin) disimpan sementara
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CORS_ALLOW_ALL_ORIGINS = True
