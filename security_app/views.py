22f6130d653752837232032bb231b9d2247f0dc2
trishagarniss <trishagarniss@gmail.com>
2026-05-08 16:58:29 +0700

    ppp

--- security_app/views.py
+++ security_app/views.py
@@ -262,5 +262,8 @@ def console_view(request):
     }
     return render(request, 'console.html', context)
 
+def architecture_view(request):
+    return render(request, 'architecture.html')
+
 def about_view(request):
     return render(request, 'about.html')
