1ef13d18ac348cc3cd98122127ae4200880da95b
trishagarniss <trishagarniss@gmail.com>
2026-05-08 10:42:10 +0700

    ..

--- cloudguard_prj/urls.py
+++ cloudguard_prj/urls.py
@@ -16,8 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from security_app import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/security/', include('security_app.urls')),
+    
+    path('', views.home_view, name='home'),
 ]
