5a0cee3561fbe8c0719b9e3c3149f3a9a5766c65
trishagarniss <trishagarniss@gmail.com>
2026-06-17 06:17:58 +0700

    done

--- cloudguard_prj/settings.py
+++ cloudguard_prj/settings.py
@@ -89,7 +89,7 @@ DATABASES = {
         'USER': os.getenv('DB_USER'),
         'PASSWORD': os.getenv('DB_PASSWORD'),
         'HOST': os.getenv('DB_HOST'),
-        'PORT': os.getenv('DB_PORT', '5432'),
+        'PORT': os.getenv('DB_PORT', '6543'),
         'OPTIONS': {
             'sslmode': 'require',
             # 'Encrypt': 'True',
